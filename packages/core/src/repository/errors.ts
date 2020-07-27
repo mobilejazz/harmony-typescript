bab33c0245c1fa06a61e11d0db8c9000e0623f63
Joan Martin <joan@mobilejazz.com>
2020-07-27 12:46:34 +0200

    Improving error class structure and error mangement in oauth

--- packages/core/src/repository/errors.ts
+++ packages/core/src/repository/errors.ts
@@ -1,82 +1,88 @@
 
 export const UnknownErrorCode = -1;
 
-export const MethodNotImplementedErrorName =  'Method Not Implemented';
-export class MethodNotImplementedError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
+export abstract class CodedError extends Error {
+    protected constructor(message?: string, public readonly code: number = UnknownErrorCode) {
         super(message);
+    }
+}
+
+export const MethodNotImplementedErrorName =  'Method Not Implemented';
+export class MethodNotImplementedError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, MethodNotImplementedError.prototype);
         this.name = MethodNotImplementedErrorName;
     }
 }
 
 export const OperationNotSupportedErrorName =  'Operation Not Supported';
-export class OperationNotSupportedError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class OperationNotSupportedError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, OperationNotSupportedError.prototype);
         this.name = OperationNotSupportedErrorName;
     }
 }
 
 export const QueryNotSupportedErrorName =  'Query Not Supported';
-export class QueryNotSupportedError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class QueryNotSupportedError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, QueryNotSupportedError.prototype);
         this.name = QueryNotSupportedErrorName;
     }
 }
 
 export const DeleteErrorName = 'Delete Error';
-export class DeleteError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class DeleteError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, DeleteError.prototype);
         this.name = DeleteErrorName;
     }
 }
 
 export const NotValidErrorName = 'Not Valid Error';
-export class NotValidError extends  Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class NotValidError extends  CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, NotValidError.prototype);
         this.name = NotValidErrorName;
     }
 }
 
 export const NotFoundErrorName = 'Not Found Error';
-export class NotFoundError extends  Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class NotFoundError extends  CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, NotFoundError.prototype);
         this.name = NotFoundErrorName;
     }
 }
 
 export const InvalidArgumentErrorName = 'Invalid Argument';
-export class InvalidArgumentError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class InvalidArgumentError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, InvalidArgumentError.prototype);
         this.name = InvalidArgumentErrorName;
     }
 }
 
 export const FailedErrorName = 'Failed Error';
-export class FailedError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class FailedError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, FailedError.prototype);
         this.name = FailedErrorName;
     }
 }
 
 export const ForbiddenErrorName = 'Forbidden Error';
-export class ForbiddenError extends Error {
-    constructor(message?: string, public readonly code: number = UnknownErrorCode) {
-        super(message);
+export class ForbiddenError extends CodedError {
+    constructor(message?: string, code: number = UnknownErrorCode) {
+        super(message, code);
         Object.setPrototypeOf(this, ForbiddenError.prototype);
         this.name = ForbiddenErrorName;
     }
--- packages/nest/src/oauth/domain/interactors/auth-controller.interactor.ts
+++ packages/nest/src/oauth/domain/interactors/auth-controller.interactor.ts
@@ -1,5 +1,28 @@
 import {Request, Response} from "oauth2-server";
 import OAuth2Server = require("oauth2-server");
+import {FailedError, ForbiddenError} from "@mobilejazz/harmony-core";
+import {BadRequestException, ForbiddenException} from "@nestjs/common";
+
+export class InvalidRefreshTokenError extends ForbiddenException {
+    constructor() {
+        super('Invalid refresh token');
+        Object.setPrototypeOf(this, InvalidRefreshTokenError.prototype);
+    }
+}
+
+export class InvalidGrantError extends BadRequestException {
+    constructor() {
+        super('Invalid grant error');
+        Object.setPrototypeOf(this, InvalidGrantError.prototype);
+    }
+}
+
+export class InvalidClientError extends BadRequestException {
+    constructor() {
+        super('Invalid client error');
+        Object.setPrototypeOf(this, InvalidClientError.prototype);
+    }
+}
 
 export class AuthControllerInteractor {
     constructor(
@@ -15,17 +38,37 @@ export class AuthControllerInteractor {
             oauthRequest.headers['content-type'] = 'application/x-www-form-urlencoded';
         }
 
-        await this.oauthServer.token(
-            oauthRequest,
-            oauthResponse,
-            null,
-            () => {
-                response.status(oauthResponse.status);
-                Object.keys(oauthResponse.headers).forEach(key => {
-                    response.setHeader(key, oauthResponse.headers[key]);
-                });
-                response.send(oauthResponse.body);
-            },
-        );
+        return new Promise<void>((resolve, reject) => {
+            this.oauthServer.token(
+                oauthRequest,
+                oauthResponse,
+                null,
+                () => {
+                    if (oauthResponse.status < 400) {
+                        response.status(oauthResponse.status);
+                        Object.keys(oauthResponse.headers).forEach(key => {
+                            response.setHeader(key, oauthResponse.headers[key]);
+                        });
+                        response.send(oauthResponse.body);
+                    }
+                },
+            ).then(result => {
+                // Successful result is already handled in the callback
+            }).catch(error => {
+                if (error.name === 'invalid_grant') {
+                    // Catching the invalid grant error from OAuth2Server
+                    reject(new InvalidGrantError());
+                } else if (error.name === 'invalid_client') {
+                    // Catching the invalid client error from OAuth2Server
+                    reject(new InvalidClientError());
+                } else if (error.message.message === 'Invalid refresh token') {
+                    // Catching the invalid refresh_token error from OAuth2Server
+                    reject(new InvalidRefreshTokenError());
+                } else {
+                    // Return whatever error otherwise
+                    reject(error);
+                }
+            });
+        });
     }
 }
