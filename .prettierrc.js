5218cdcdf560204f70ffd1c00dfac3a719b9f95e
doup <asier@illarra.com>
2021-09-16 10:12:57 +0200

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false, // default
+    semi: true, // default
+    singleQuote: true,
+    quoteProps: 'as-needed', // default
+    trailingComma: 'all',
+    bracketSpacing: true, // default
+    bracketSameLine: true,
+    arrowParens: 'always', // default
+    proseWrap: 'preserve', // default
+    htmlWhitespaceSensitivity: 'css', // default
+    endOfLine: 'lf', // default
+    embeddedLanguageFormatting: 'auto', // default
+};
