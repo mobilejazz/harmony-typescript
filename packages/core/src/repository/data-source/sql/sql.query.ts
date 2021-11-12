f22660e217f76cdfddba17ab13d6a669c67b8c47
doup <asier@illarra.com>
2021-11-12 12:05:52 +0100

    Remove unused parameters & add missing method visibility keywords

--- packages/core/src/repository/data-source/sql/sql.query.ts
+++ packages/core/src/repository/data-source/sql/sql.query.ts
@@ -4,35 +4,35 @@ import { BaseColumnCreatedAt } from './sql.constants';
 import { SQLDialect } from '../../../data';
 
 export abstract class SQLOrderByQuery extends Query implements SQLOrderBy {
-    abstract orderBy(param: SQLQueryParamFn, dialect: SQLDialect): string;
-    abstract ascending(): boolean;
+    public abstract orderBy(param: SQLQueryParamFn, dialect: SQLDialect): string;
+    public abstract ascending(): boolean;
 }
 
 export abstract class SQLOrderByPaginationQuery extends PaginationOffsetLimitQuery implements SQLOrderBy {
-    abstract orderBy(param: SQLQueryParamFn, dialect: SQLDialect): string;
-    abstract ascending(): boolean;
+    public abstract orderBy(param: SQLQueryParamFn, dialect: SQLDialect): string;
+    public abstract ascending(): boolean;
 }
 
 export abstract class SQLWhereQuery extends SQLOrderByQuery implements SQLWhere {
-    abstract where(param: SQLQueryParamFn, dialect: SQLDialect): string;
+    public abstract where(param: SQLQueryParamFn, dialect: SQLDialect): string;
 
-    orderBy(_param: SQLQueryParamFn, _dialect: SQLDialect): string {
+    public orderBy(): string {
         return BaseColumnCreatedAt;
     }
 
-    ascending(): boolean {
+    public ascending(): boolean {
         return false;
     }
 }
 
 export abstract class SQLWherePaginationQuery extends SQLOrderByPaginationQuery implements SQLWhere {
-    abstract where(param: SQLQueryParamFn, dialect: SQLDialect): string;
+    public abstract where(param: SQLQueryParamFn, dialect: SQLDialect): string;
 
-    orderBy(_param: SQLQueryParamFn, _dialect: SQLDialect): string {
+    public orderBy(): string {
         return BaseColumnCreatedAt;
     }
 
-    ascending(): boolean {
+    public ascending(): boolean {
         return false;
     }
 }
