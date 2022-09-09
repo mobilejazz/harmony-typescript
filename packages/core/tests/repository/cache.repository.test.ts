737ecbbe2e3658fb375d7a42e9987ab78eaab925
Alex-DA <Alex-DA@users.noreply.github.com>
2022-09-09 14:22:02 +0200

    update last modification

--- packages/core/tests/repository/cache.repository.test.ts
+++ packages/core/tests/repository/cache.repository.test.ts
@@ -1,11 +1,18 @@
 import {
+    CacheOperation,
     CacheRepository,
+    CacheSyncOperation,
     DefaultObjectValidator,
     DefaultOperation,
     InMemoryDataSource,
     KeyQuery,
+    MainOperation,
+    MainSyncOperation,
+    NotFoundError,
     Operation,
     OperationNotSupportedError,
+    Query,
+    QueryNotSupportedError,
 } from '../../src';
 import {Book, getDefaultBook, getRandomBook} from './BookHelper';
 
@@ -48,6 +55,26 @@ describe('CacheRepository', () => {
 
             return expect(repository.get(query, operation)).resolves.toEqual(book);
         });
+
+        it('On existing Key on Main DataSource return the Value', () => {
+            expect.assertions(1);
+            const query = getDefaultKeyQuery();
+            const operation = getMainOperation();
+            const book = getDefaultBook();
+            repository.put(book, query, operation);
+
+            return expect(repository.get(query, operation)).resolves.toEqual(book);
+        });
+
+        it('On existing Key on Cache DataSource return the Value', () => {
+            expect.assertions(1);
+            const query = getDefaultKeyQuery();
+            const operation = getCacheOperation();
+            const book = getDefaultBook();
+            repository.put(book, query, operation);
+
+            return expect(repository.get(query, operation)).resolves.toEqual(book);
+        });
     });
 
     describe('put', () => {
@@ -75,6 +102,15 @@ describe('CacheRepository', () => {
             repository.put(bookTwo, query, operation);
 
             return expect(repository.get(query, operation)).resolves.toEqual(bookTwo);
+        });
+
+        it('On MainSync Error not update Cache DataSource', async () => {
+            expect.assertions(2);
+
+            await expect(repository.put(getDefaultBook(), getNotSupportedQuery(), getMainSyncOperation()))
+                .rejects
+                .toThrow(QueryNotSupportedError);
+            await expect(repository.get(getDefaultKeyQuery(), getCacheOperation())).rejects.toThrow(NotFoundError);
         });
     });
 
@@ -122,10 +158,30 @@ function getNotSupportedOperation(): Operation {
     return new WrongOperation();
 }
 
+function getMainOperation(): Operation {
+    return new MainOperation();
+}
+
+function getCacheOperation(): Operation {
+    return new CacheOperation();
+}
+
+function getMainSyncOperation(): Operation {
+    return new MainSyncOperation();
+}
+
+function getCacheSyncOperation(): Operation {
+    return new CacheSyncOperation();
+}
+
 function getDefaultKeyQuery(): KeyQuery {
     return new KeyQuery('bookOne');
 }
 
 function getNotExistingKeyQuery(): KeyQuery {
     return new KeyQuery('some key that not exist');
+}
+
+function getNotSupportedQuery(): Query {
+    return new Query();
 }
