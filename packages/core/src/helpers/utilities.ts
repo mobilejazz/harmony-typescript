8f07780a1a45ae9526d9d9bfd53810aa28bcd1fc
Asier Illarramendi <asier@illarra.com>
2022-10-01 10:45:55 +0200

    Add `CacheDecoratorFactory` & `UnreachableCaseError` utilities

---
+++ packages/core/src/helpers/utilities.ts
@@ -0,0 +1,32 @@
+type CacheDecorator = {
+    (): ((target: unknown, propertyKey: string, descriptor: PropertyDescriptor) => void);
+    cache: Map<string, unknown>;
+};
+
+export function CacheDecoratorFactory(): CacheDecorator {
+    const cache = new Map<string, unknown>();
+    const decorator = function CacheDecorator() {
+        return function (_: unknown, propertyKey: string, descriptor: PropertyDescriptor): void {
+            const method = descriptor.value;
+
+            descriptor.value = function () {
+                if (!cache.has(propertyKey)) {
+                    cache.set(propertyKey, method.apply(this));
+                }
+
+                return cache.get(propertyKey);
+            };
+        }
+    }
+
+    // Expose the `Map` cache
+    decorator.cache = cache;
+
+    return decorator;
+}
+
+export class UnreachableCaseError extends Error {
+    constructor(val: never) {
+        super(`Unreachable case: ${JSON.stringify(val)}`);
+    }
+}
