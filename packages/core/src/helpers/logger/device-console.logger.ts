88fe70e82525492090e2e10d5bbf24b8376f95c0
doup <asier@illarra.com>
2022-12-12 10:42:58 +0100

    Add extra documentation in `DeviceConsoleLogger.handleLog`

--- packages/core/src/helpers/logger/device-console.logger.ts
+++ packages/core/src/helpers/logger/device-console.logger.ts
@@ -24,11 +24,22 @@ export class DeviceConsoleLogger extends Logger {
         if (this.tag) {
             const tagStr = `[${this.tag}]`;
 
+            // Our API is compatible with `console.*` methods this means that there are two ways of using this API:
+            //
+            // - With an array of mixed values
+            // - With a template as first parameter: `[string, ...unknown[]]`. See: https://developer.mozilla.org/en-US/docs/Web/API/console#using_string_substitutions
+            //
+            // This affects how we handle the "tag". If it's an array of mixed values, we can simply prepend
+            // the tag to the `parameters` array. But, if we're using the template signature we don't want to mess
+            // with the template, that's why we prepend the tag to the template string. This way the first parameter
+            // is still (potentially) a template.
             if (parameters.length === 0 || typeof parameters[0] !== 'string') {
-                // Set tag as first parameters array element
+                // CASE: array of mixed values
+                // Add tag in the first position of the parameters array
                 parameters.unshift(tagStr);
             } else {
-                // Mix tag with parameter as it might have formatting placeholders
+                // CASE: Template with string substitutions
+                // Add tag to the first parameter as it might have formatting placeholders
                 parameters[0] = `${tagStr} ${parameters[0]}`;
             }
         }
