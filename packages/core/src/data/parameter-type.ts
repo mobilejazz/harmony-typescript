aba4c69459a9fecef5dfd4a405000e499a10b3bc
Oriol Janés <urijp@rog>
2023-03-06 22:07:19 +0100

    types simplification

--- packages/core/src/data/parameter-type.ts
+++ packages/core/src/data/parameter-type.ts
@@ -1,2 +1,3 @@
-export type HttpParameter = Record<string, ParameterType>;
+export type HttpParameters = Record<string, ParameterType>;
+export type HttpBody = Record<string, ParameterType | unknown | unknown[]> | undefined;
 export type ParameterType = string | string[] | number | number[] | boolean | boolean[];
